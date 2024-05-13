ed3e821e4e180ab6528c34482a1b9a23c3454fbd
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-05-13 16:38:30 +0800

    Update gpt4.js

--- script/gpt4.js
+++ script/gpt4.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 module.exports.config = {
-  name: 'gpt4',
+  name: 'ai',
   version: '1.0.0',
   role: 0,
-  hasPrefix: true,
+  hasPrefix: false,
   aliases: ['gpt', 'openai'],
   description: "An AI command powered by GPT-4",
-  usage: "gpt4 [prompt]",
+  usage: "Ai [promot]",
   credits: 'Developer',
   cooldown: 3,
 };
@@ -20,11 +20,13 @@ module.exports.run = async function({
     api.sendMessage(`Please provide a question or statement after 'ai'. For example: 'ai What is the capital of France?'`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`🔍 Generating your Prompt Using GPT4...`, event.threadID, event.messageID);
+  api.sendMessage(`🔍 "${input}"`, event.threadID, event.messageID);
   try {
     const {
       data
-    } = await axios.get(`https://akhiro-rest-api.onrender.com/api/gpt4?q=${encodeURIComponent(input)}`)
+    } = await axios.get(`https://joshweb.click/new/gpt-4_adv?prompt=${encodeURIComponent(input)}`);
+    const response = data.response;
+    api.sendMessage(response + '\n\nhttps://www.facebook.com/61557924257806', event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
