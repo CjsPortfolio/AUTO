0537208936684357eecb5f7f4ccf4ff54ff36c3a
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-05-13 15:19:52 +0800

    Update and rename ai.js to gpt4.js

--- script/gpt4.js
+++ script/gpt4.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 module.exports.config = {
-  name: 'ai',
+  name: 'gpt4',
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
   aliases: ['gpt', 'openai'],
   description: "An AI command powered by GPT-4",
-  usage: "Ai [promot]",
+  usage: "gpt4 [prompt]",
   credits: 'Developer',
   cooldown: 3,
 };
@@ -20,13 +20,11 @@ module.exports.run = async function({
     api.sendMessage(`Please provide a question or statement after 'ai'. For example: 'ai What is the capital of France?'`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`🔍 "Generating your Prompt Using GPT4..."`, event.threadID, event.messageID);
+  api.sendMessage(`🔍 Generating your Prompt Using GPT4...`, event.threadID, event.messageID);
   try {
     const {
       data
-    } = await axios.get(`https://joshweb.click/new/gpt-4_adv?prompt=${encodeURIComponent(input)}`);
-    const response = data.response;
-    api.sendMessage(response + '\n\nhttps://bit.ly/create-chatbot-me', event.threadID, event.messageID);
+    } = await axios.get("GPT-4: \n", `https://joshweb.click/new/gpt-4_adv?prompt=${encodeURIComponent(input)}`)
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
