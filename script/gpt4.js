cf23ef523aec17d511cdd36b2caf5f89d8aba8b6
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-05-13 17:52:55 +0800

    Update and rename ai.js to gpt4.js

---
+++ script/gpt4.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+module.exports.config = {
+    name: "gpt4",
+    version: "1.0.0",
+    description: "AI Powered by GPT-4",
+    usage: "gpt4 [message]",
+    author: "Rui",
+    cooldown: 5,
+    hasPrefix: true,
+    role: 0,
+  }; module.exports(.)run async function({ fonts, api, message, args }) {
+    const query = args.join(" ");
+
+    if (!query) {
+      message.reply("❌ | Please provide a query!");
+    } else {
+      const info = await
+message.reply(`🔍Please wait...`);
+      const response = await axios.get(`https://akhiro-rest-api.onrender.com/api/gpt4?q=${encodeURIComponent(query)}`);
+      api.editMessage(
+        `${fonts.bold("🤖 | GPT-4")}\n━━━━━━━━━━━━━━━\n${response.data.content}`,
+        info.messageID,
+      );
+    }
+  },
+};
